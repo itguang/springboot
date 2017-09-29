e68bf18365a1772b6b92bed28ae83be5263557d2
itguang <1445037803@qq.com>
2017-09-29 10:26:16 +0800

    热部署配置成功!

--- src/main/java/com/yearcon/girl/GirlApplication.java
+++ src/main/java/com/yearcon/girl/GirlApplication.java
@@ -1,6 +1,5 @@
 package com.yearcon.girl;
 
-import com.yearcon.girl.config.JdbcConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -9,11 +8,21 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class GirlApplication {
 
 	public static void main(String[] args) {
+//		SpringApplication.run(GirlApplication.class, args);
+//
+//		ConfigurableApplicationContext context = SpringApplication.run(GirlApplication.class, args);
+//		context.getBean(JdbcConfig.class).show();
 
 
-		ConfigurableApplicationContext context = SpringApplication.run(GirlApplication.class, args);
-		context.getBean(JdbcConfig.class).show();
+		SpringApplication app = new SpringApplication(GirlApplication.class);
 
+		//可以通过启动参数的方式配置生效的 profile: --spring.profiles.active=test
+//		app.setAdditionalProfiles("test");//编程方式设置生效的 profile,默认的配置文件也能生效
+
+		ConfigurableApplicationContext context = app.run(args);
+		String msg = context.getEnvironment().getProperty("dev.msg");//得到配置文件中的属性
+		System.out.println(msg);
+		System.out.println(context.getEnvironment().getProperty("server.port"));
 
 
 	}
